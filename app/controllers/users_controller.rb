ac2ea2689d4652144ff8510a78bc6c6146c9a44a
hhassan <hassanhamza0101@gmail.com>
2021-10-23 03:38:58 +0500

    User model generated

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: { 'Error': @user.errors.full_message }
+    end
+  end
+
+  def update
+  end
+  
+  def delete
+    @user = User.find_by(params[:id])
+    @user&.destroy
+  end
+
+  private 
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
